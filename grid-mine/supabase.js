1569e774de8bc3b85e56efce5b1e5cc5b4ff8853
Bautistagl <bautistagonzalezlazo@gmail.com>
2024-02-20 15:15:41 -0300

    dsadsa

--- grid-mine/supabase.js
+++ grid-mine/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://avkhptsnurrgobwwomgx.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2a2hwdHNudXJyZ29id3dvbWd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY2NjQ3MDAsImV4cCI6MjAyMjI0MDcwMH0.RC1-JzrWQ8CtNfo5er_82u-OQSEvhiJkDIz0KUygcks'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
