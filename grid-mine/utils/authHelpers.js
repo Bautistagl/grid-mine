8c309e88b5cd4535066dcb3b8e993e073cb88a49
Bautistagl <bautistagonzalezlazo@gmail.com>
2024-02-20 15:08:55 -0300

    dsdas

--- grid-mine/utils/authHelpers.js
+++
@@ -1,6 +0,0 @@
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-
-export const supabaseClient = createClientComponentClient({
-  cookies: () => cookies(),
-});
